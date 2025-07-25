7498f8b4c0e1a5e5cfcf44296aefa0f6bb2c900b
KYOGRE-LS <luisrecelly09@gmail.com>
2025-07-25 00:09:29 -0300

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    return render_template("tutorial.html")
 
 @app.route("/get_response", methods=["POST"])
 def chatbot_response():
